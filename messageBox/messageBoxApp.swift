d52551143bd9aee1b5cd4f0db3d8a1a6512769d0
Donal Higgins <dvh121103@gmail.com>
2021-12-24 15:16:27 -0600

    Initial Commit

---
+++ messageBox/messageBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  messageBoxApp.swift
+//  messageBox
+//
+//  Created by Donal Higgins on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct messageBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
