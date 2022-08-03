1505441495d663ae6309cd0399f8cb458fcf9bc6
Donal Higgins <dvh121103@gmail.com>
2022-08-03 12:00:43 -0500

    Minor Updates

--- iOS App/messageBox/ContentView.swift
+++ iOS App/messageBox/ContentView.swift
@@ -9,263 +9,6 @@ import CocoaMQTT
 import Foundation
 import Alamofire
 
-public extension UIImage {
-
-    var pixelWidth: Int {
-        return cgImage?.width ?? 0
-    }
-
-    var pixelHeight: Int {
-        return cgImage?.height ?? 0
-    }
-
-    func pixelColor(x: Int, y: Int) -> UIColor {
-        assert(
-            0..<pixelWidth ~= x && 0..<pixelHeight ~= y,
-            "Pixel coordinates are out of bounds")
-
-        guard
-            let cgImage = cgImage,
-            let data = cgImage.dataProvider?.data,
-            let dataPtr = CFDataGetBytePtr(data),
-            let colorSpaceModel = cgImage.colorSpace?.model,
-            let componentLayout = cgImage.bitmapInfo.componentLayout
-        else {
-            assertionFailure("Could not get a pixel of an image")
-            return .clear
-        }
-
-        assert(
-            colorSpaceModel == .rgb,
-            "The only supported color space model is RGB")
-        assert(
-            cgImage.bitsPerPixel == 32 || cgImage.bitsPerPixel == 24,
-            "A pixel is expected to be either 4 or 3 bytes in size")
-
-        let bytesPerRow = cgImage.bytesPerRow
-        let bytesPerPixel = cgImage.bitsPerPixel/8
-        let pixelOffset = y*bytesPerRow + x*bytesPerPixel
-
-        if componentLayout.count == 4 {
-            let components = (
-                dataPtr[pixelOffset + 0],
-                dataPtr[pixelOffset + 1],
-                dataPtr[pixelOffset + 2],
-                dataPtr[pixelOffset + 3]
-            )
-
-            var alpha: UInt8 = 0
-            var red: UInt8 = 0
-            var green: UInt8 = 0
-            var blue: UInt8 = 0
-
-            switch componentLayout {
-            case .bgra:
-                alpha = components.3
-                red = components.2
-                green = components.1
-                blue = components.0
-            case .abgr:
-                alpha = components.0
-                red = components.3
-                green = components.2
-                blue = components.1
-            case .argb:
-                alpha = components.0
-                red = components.1
-                green = components.2
-                blue = components.3
-            case .rgba:
-                alpha = components.3
-                red = components.0
-                green = components.1
-                blue = components.2
-            default:
-                return .clear
-            }
-
-            // If chroma components are premultiplied by alpha and the alpha is `0`,
-            // keep the chroma components to their current values.
-            if cgImage.bitmapInfo.chromaIsPremultipliedByAlpha && alpha != 0 {
-                let invUnitAlpha = 255/CGFloat(alpha)
-                red = UInt8((CGFloat(red)*invUnitAlpha).rounded())
-                green = UInt8((CGFloat(green)*invUnitAlpha).rounded())
-                blue = UInt8((CGFloat(blue)*invUnitAlpha).rounded())
-            }
-
-            return .init(red: red, green: green, blue: blue, alpha: alpha)
-
-        } else if componentLayout.count == 3 {
-            let components = (
-                dataPtr[pixelOffset + 0],
-                dataPtr[pixelOffset + 1],
-                dataPtr[pixelOffset + 2]
-            )
-
-            var red: UInt8 = 0
-            var green: UInt8 = 0
-            var blue: UInt8 = 0
-
-            switch componentLayout {
-            case .bgr:
-                red = components.2
-                green = components.1
-                blue = components.0
-            case .rgb:
-                red = components.0
-                green = components.1
-                blue = components.2
-            default:
-                return .clear
-            }
-
-            return .init(red: red, green: green, blue: blue, alpha: UInt8(255))
-
-        } else {
-            assertionFailure("Unsupported number of pixel components")
-            return .clear
-        }
-    }
-
-}
-
-public extension UIColor {
-
-    convenience init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) {
-        self.init(
-            red: CGFloat(red)/255,
-            green: CGFloat(green)/255,
-            blue: CGFloat(blue)/255,
-            alpha: CGFloat(alpha)/255)
-    }
-
-}
-
-public extension CGBitmapInfo {
-
-    enum ComponentLayout {
-
-        case bgra
-        case abgr
-        case argb
-        case rgba
-        case bgr
-        case rgb
-
-        var count: Int {
-            switch self {
-            case .bgr, .rgb: return 3
-            default: return 4
-            }
-        }
-
-    }
-
-    var componentLayout: ComponentLayout? {
-        guard let alphaInfo = CGImageAlphaInfo(rawValue: rawValue & Self.alphaInfoMask.rawValue) else { return nil }
-        let isLittleEndian = contains(.byteOrder32Little)
-
-        if alphaInfo == .none {
-            return isLittleEndian ? .bgr : .rgb
-        }
-        let alphaIsFirst = alphaInfo == .premultipliedFirst || alphaInfo == .first || alphaInfo == .noneSkipFirst
-
-        if isLittleEndian {
-            return alphaIsFirst ? .bgra : .abgr
-        } else {
-            return alphaIsFirst ? .argb : .rgba
-        }
-    }
-
-    var chromaIsPremultipliedByAlpha: Bool {
-        let alphaInfo = CGImageAlphaInfo(rawValue: rawValue & Self.alphaInfoMask.rawValue)
-        return alphaInfo == .premultipliedFirst || alphaInfo == .premultipliedLast
-    }
-
-}
-
-func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage {
-    let newHeight = newWidth
-    UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
-    image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
-    let newImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    return newImage!     }
-
-func getArrayOfBytesFromImage(imageData:NSData) -> Array<UInt8>
-{
-  // the number of elements:
-  let count = imageData.length / MemoryLayout<Int8>.size
-
-  // create array of appropriate length:
-  var bytes = [UInt8](repeating: 0, count: count)
-
-  // copy bytes into array
-  imageData.getBytes(&bytes, length:count * MemoryLayout<Int8>.size)
-
-  var byteArray:Array = Array<UInt8>()
-
-  for i in 0 ..< count {
-    byteArray.append(bytes[i])
-  }
-
-  return byteArray
-}
-
-
-func runLengthEncoding(str: String) -> String {
-    var out = ""
-    let len = str.count
-    var cnt = 0
-    var i = 0
-    while(i < len) {
-        cnt = 1
-        while(i < len - 1 && str[i] == str[i + 1]) {
-            cnt += 1
-            i += 1
-        }
-        
-        out += String(str[i]) + ":" + String(cnt) + ","
-        
-        i += 1
-    }
-    return out
-}
-
-
-extension UIColor {
-    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        var alpha: CGFloat = 0
-        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-
-        return (red, green, blue, alpha)
-    }
-}
-
-func imgToBinaryArray(image: UIImage) -> String {
-    var binArray = ""
-    var color: UIColor
-    var grayScale: Float
-    var red = image.pixelColor(x: 0, y: 0).rgba
-    for y in 0...127 {
-        for x in 0...127 {
-            color = image.pixelColor(x: x, y: y)
-            red = color.rgba
-
-            grayScale = Float(0.299 * red.red + 0.587 * red.green + 0.144 * red.blue)
-            if(grayScale >= 0.5){
-                binArray += "1"
-            }
-            else{
-                binArray += "0"
-            }
-        }
-    }
-    return binArray
-}
 
 
 public enum DispatchLevel {
@@ -326,13 +69,13 @@ struct ContentView: View {
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 10).fill(Color(hue: 0.924, saturation: 1.0, brightness: 1.0, opacity: 0.1)))
                 Button("Send") {
-                    /*
+                    
                     AF.request(url, method: .post, parameters: data)
                         .authenticate(username: accountSID, password: authToken)
                         .responseJSON { response in
                             debugPrint(response)
                         }
-                    */
+                    
                     if(pictureMode){
                         
                         inputImage = resizeImage(image: inputImage!, newWidth: 128)
@@ -345,13 +88,6 @@ struct ContentView: View {
                         
                         self.mqttClient.publish("", withString: String(message1))
                         self.mqttClient.publish("", withString: String(message2))
-
-                        
-
-
-                        
-
-                        
                         
                     }
                     else{
@@ -392,6 +128,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
 
